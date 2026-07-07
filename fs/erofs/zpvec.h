94baeb5390e723f8499aad652cd1685148a4fee8
yuksk-cyber <emir.bekmuradow2012@gmail.com>
2026-07-07 15:10:09 +0500

    fs: erofs: zpvec.h: rework

--- fs/erofs/zpvec.h
+++ fs/erofs/zpvec.h
@@ -1,7 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (C) 2018 HUAWEI, Inc.
- *             https://www.huawei.com/
+ *             http://www.huawei.com/
+ * Created by Gao Xiang <gaoxiang25@huawei.com>
  */
 #ifndef __EROFS_FS_ZPVEC_H
 #define __EROFS_FS_ZPVEC_H
