de8debedb49fa56fc93221c034e820b1863aa06f
yuksk-cyber <emir.bekmuradow2012@gmail.com>
2026-07-30 15:36:20 +0500

    erofs: move from drivers/staging to fs/

--- fs/erofs/tagptr.h
+++ fs/erofs/tagptr.h
@@ -1,8 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * A tagged pointer implementation
- *
- * Copyright (C) 2018 Gao Xiang <gaoxiang25@huawei.com>
  */
 #ifndef __EROFS_FS_TAGPTR_H
 #define __EROFS_FS_TAGPTR_H
